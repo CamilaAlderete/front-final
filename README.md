72a14cf999a01d413f060cbc35a7cd4272b863b7
CamiAlderete <alderetecami996@gmail.com>
2022-11-22 12:54:37 -0300

    base

---
+++ README.md
@@ -0,0 +1,22 @@
+# Primera parcial de Frontend
+
+Proyecto generado con [Angular CLI](https://github.com/angular/angular-cli) version 13.1.4.
+
+## Equipo
+- Osvaldo Caje
+- Soledad Ayala
+- Monica Auler 
+- Camila Alderete
+
+## Instalar librerias 
+
+Correr `npm install` en la terminal del proyecto.
+
+## Servidor de desarrollo
+
+Correr `ng serve` e ir a `http://localhost:4200/`.
+
+
+## Crear componente
+
+Correr `ng generate component directorio-componente-de-prueba/componente-de-prueba` o `ng g c directorio-componente-de-prueba/componente-de-prueba`
---
+++ src/app/app.module.ts
@@ -0,0 +1,77 @@
+import {LOCALE_ID, NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+
+import { LayoutModule } from '@angular/cdk/layout';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import {MatTableDataSource, MatTableModule} from "@angular/material/table";
+import {MatFormFieldModule, MatLabel} from "@angular/material/form-field";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import { CositasUtilesComponent } from './cositas-utiles/cositas-utiles/cositas-utiles.component';
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatDialogModule, MatDialogRef } from "@angular/material/dialog";
+import {MatSelectModule} from '@angular/material/select';
+import {MatListModule} from "@angular/material/list";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatSortModule} from "@angular/material/sort";
+
+
+@NgModule({
+  //componentes creados
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CositasUtilesComponent,
+  ],
+  // librerias y modulos de angular y librerias externas
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        LayoutModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatIconModule,
+        MatDividerModule,
+        HttpClientModule,
+        ToastrModule.forRoot(),
+        MatTableModule,
+        FormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatDialogModule,
+        MatSelectModule,
+        MatListModule,
+        MatPaginatorModule,
+        MatSortModule,
+        ReactiveFormsModule,
+        MatDialogModule,
+    ],
+  providers: [
+    //para usar datepicker
+    MatDatepickerModule,
+    MatNativeDateModule,
+    {
+      provide: MatDialogRef,
+      useValue: {}
+    },
+    { provide: LOCALE_ID, useValue: 'en-nz' },
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
